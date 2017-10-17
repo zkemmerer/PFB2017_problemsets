e4ad9a864ed5f88f355ace4dac60409e5854da63
admin <admin@pfb05.cshl.edu>
2017-10-17 12:16:28 -0400

    171017 Python 1 PS

---
+++ importsys.scripts2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+import sys
+Zack = sys.argv[1]
+red = sys.argv[2]
+ninja = sys.argv[3]
+cheetah = sys.argv[4]
+print (Zack, red, ninja, cheetah)
---
+++ myscript.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+a = "Zack"
+b = "red"
+c = "ninja"
+d = "cheetah"
+print (a,b,c,d)
